dc9ab7738b43175df4378f65ee8d6ece595adf19
Wagner Hélio <wagner.whsf@gmail.com>
2024-11-23 14:55:31 -0300

    Update lama3.2-vision.py

--- lama3.2-vision.py
+++ lama3.2-vision.py
@@ -1,12 +1,14 @@
 import ollama
 
-response = ollama.chat(
-    model='llama3.2-vision',
-    messages=[{
-        'role': 'user',
-        'content': 'O que tem nessa imagem?',
-        'images': ['image.jpeg']
-    }]
-)
-
-print(response)
+try:
+    response = ollama.chat(
+        model='llama3.2-vision',
+        messages=[{
+            'role': 'user',
+            'content': 'O que tem nessa imagem?',
+            'images': ['image.jpeg']
+        }]
+    )
+    print(response)
+except Exception as e:
+    print(f"Erro: {e}")
